170c527ef0d997479baefc7904a2b3e654f24247
abrahan-tl <abrahan-tl>
2018-08-02 20:45:09 -0500

    logica alterna
    logica alterna

--- Vigilante/Assets/Vigilante/Scrip/Enemy.cs
+++ Vigilante/Assets/Vigilante/Scrip/Enemy.cs
@@ -16,8 +16,9 @@ public class Enemy : MonoBehaviour {
     Vector3 uiltima_posicion;
     bool visto;
     public GameObject hero;
-
-
+    public float distancia = 5;
+    bool mira;
+    bool perimetro;
 	// Use this for initialization
 	void Start () {
         timer = 0;
@@ -29,8 +30,6 @@ public class Enemy : MonoBehaviour {
         positions[3] = new Vector3(22,1,-22);
 
         estado = Estados.Patrullando;
-    
-
 	}
 
     public void Awake()
@@ -40,27 +39,28 @@ public class Enemy : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Perimetro();
+        
         switch (estado)
         {
             case Estados.Patrullando:
                 Patrullar();
                 break;
             case Estados.Esperando:
+               
                 Esperar();
                 break;
             case Estados.Persiguiendo:
                 Perseguir();
                 break;
-
         }
-
-        //patrullar();
 	}
 
     void Patrullar()
     {
-       
+       if( Perimetro())
+        {
+            Apuntar();
+        }
         if (pos <4)
         {
             if ((positions[pos] - transform.position).magnitude <= 1)
@@ -70,20 +70,18 @@ public class Enemy : MonoBehaviour {
             nav.SetDestination(positions[pos]);
         }
         else
-        {
-            //timer = 0f;
+        {        
+            timer = 0f;
             nav.speed = 0f;
             estado = Estados.Esperando;
-        }
-
-        
-       
+            
+        }     
     }
-
+    
     void Esperar()
-    {
+    { 
         timer = timer + Time.deltaTime;
-        if (timer  >= 5)
+        if (timer >= 5)
         {
             nav.speed = 10;
             pos = 0;
@@ -94,38 +92,54 @@ public class Enemy : MonoBehaviour {
 
     void Perseguir()
     {
+        disparar();
         nav.SetDestination(uiltima_posicion);
-        //if ((hero.transform.position-transform.position).magnitude <= 10)
-        //{
-           
-        //}
-        //else
-        //{
-        //    estado = Estados.Patrullando;
-        //}
+        if((uiltima_posicion - transform.position).magnitude <1)
+        {
+            estado = Estados.Patrullando;
+        }
     }
 
-    public void Perimetro()
+    public bool Perimetro()
     {
-
-        
-
         if ((hero.transform.position - transform.position).magnitude <= 10)
         {
-            transform.LookAt(hero.transform);
-            RaycastHit perimetro;
-            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out perimetro, 10f);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10f, Color.red);
-            
-            if (perimetro.collider != null && perimetro.transform.tag == "Hero")
-            {
-                uiltima_posicion = hero.transform.position;
-                estado = Estados.Persiguiendo;
-            }           
+            return true;       
         }
         else
         {
-            estado = Estados.Patrullando;
+               return false; 
+        }      
+   }
+
+    public void disparar()
+    {
+        RaycastHit disparo;
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out disparo, distancia);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distancia, Color.black);
+        if (disparo.collider != null && disparo.transform.tag == "Hero")
+        {
+
+        }
+        D
+    }
+
+    public  bool Apuntar()
+    {
+        transform.LookAt(hero.transform);
+        RaycastHit perimetro;
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out perimetro, 10f);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10f, Color.red);
+        
+        if (perimetro.collider != null && perimetro.transform.tag == "Hero")
+        {
+            uiltima_posicion = hero.transform.position;
+            estado = Estados.Persiguiendo;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
  }
--- Vigilante/Assets/Vigilante/Scrip/Weapon.cs
+++ Vigilante/Assets/Vigilante/Scrip/Weapon.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
 
     int amon;
-    public float distance;
+    public float distance = 50;
 	// Use this for initialization
 	void Start () {
         amon = 10;
