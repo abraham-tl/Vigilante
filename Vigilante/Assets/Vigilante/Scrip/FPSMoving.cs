31beb8f96ef3395adfe647f74aec823954589872
abraham-tl <abraham-tl>
2018-08-10 20:41:06 -0500

    branch_animacion
    branch_animacion

--- Vigilante/Assets/Vigilante/Scrip/FPSMoving.cs
+++ Vigilante/Assets/Vigilante/Scrip/FPSMoving.cs
@@ -4,53 +4,49 @@ using UnityEngine;
 
 public class FPSMoving : MonoBehaviour
 {
-    private float speed = 15f;
-    private float speedd = 30f;
-
+    public  float speed = 5f;
+    float spedd;
+    Animator anim;
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = (spedd*2);
+        }
+        else
+        {
+            speed = spedd;
+        }
         MovinPlayer();
+    }
+    private void Start()
+    {
+        spedd = speed;
+        anim = GetComponent<Animator>();
     }
 
     public void MovinPlayer()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            anim.SetTrigger("walk");
             transform.position += transform.forward * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position -= transform.forward * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= transform.right * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += transform.right * speed * Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        else
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.position += transform.forward * speedd * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.position -= transform.forward * speedd * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.position -= transform.right * speedd * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.position += transform.right * speedd * Time.deltaTime;
-            }
+            anim.SetTrigger("idle");
         }
-
-
     }
 }
